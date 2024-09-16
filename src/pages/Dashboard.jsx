06b1292b4c6082d83e2fb107c95c9d86cc72d5ea
gqiqava <giogioqiqava777@gmail.com>
2024-09-16 17:53:37 +0400

    feat (dashboard) : cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,63 @@
 import styles from "./pages.module.scss";
 
 import { useState } from "react";
-import Navigation from "@/components/func/Navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Dashboard() {
-  const [form, setForm] = useState({
-    username: "",
-    password: "",
-  });
+  const [cards, setCards] = useState([
+    {
+      title: "All Buckets",
+      description: "Card Description",
+      content: "some content here",
+      footer: "card footer",
+    },
+    {
+      title: "Usage",
+      description: "Card Description",
+      content: "some content here",
+      footer: "card footer",
+    },
+    {
+      title: "Total Objects",
+      description: "Card Description",
+      content: "some content here",
+      footer: "card footer",
+    },
+  ]);
 
-  return <div>Dashboard</div>;
+  return (
+    <div style={{ padding: "20px" }}>
+      <div className={styles.dashCardContainer}>
+        {cards.map((card) => (
+          <div style={{ maxWidth: "25%", minWidth: "400px" }}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{card.title}</CardTitle>
+                <CardDescription>
+                  Card Description Lorem ipsum dolor, sit amet consectetur
+                  adipisicing elit. Suscipit inventore laudantium illum dolor
+                  quo laboriosam voluptatum tenetur consectetur! Quis ratione
+                  delectus omnis error id debitis, illo odit! Inventore, minima
+                  a.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              {/* <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter> */}
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
